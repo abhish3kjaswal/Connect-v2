9418770b0aa2c495011752427f78d338033f484c
Abhishek jaswal <abhishek.jaswal.1999@gmail.com>
2020-07-27 00:07:44 +0530

    final commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/db');
 const app = express();
 
 const mongoose = require('mongoose');
+const path = require('path');
 
 //connect database
 connectDB();
@@ -22,11 +23,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 
 app.use('/api/posts', require('./routes/api/posts'));
 
-const PORT = process.env.PORT || 5000;
+//Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
 
-// app.get('/', (req, res) => {
-//   res.send('API Running');
-// });
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server started at ${PORT}`);
